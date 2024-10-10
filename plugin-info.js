31afe14e437cc851a4681806fd245e943b3526af
Clirim <90289256+ClirimKurti@users.noreply.github.com>
2024-10-10 15:37:46 +0200

    Create plugin-info.js

---
+++ plugin-info.js
@@ -0,0 +1,15 @@
+{
+  "name": "Interactive Map",
+  "slug": "interactive-map",
+  "version": "1.5",
+  "author": "Clirim Kurti",
+  "download_url": "https://github.com//ClirimKurti/interactive-map/releases/download/interactive-map/interactive-map.8.zip",
+  "requires": "5.0",
+  "tested": "6.0",
+  "last_updated": "2024-10-10",
+  "sections": {
+    "description": "Plugin description",
+    "installation": "Installation instructions",
+    "changelog": "Changelog"
+  }
+}
